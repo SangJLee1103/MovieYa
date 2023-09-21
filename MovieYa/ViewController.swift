67aa475291c431ae45730d2174e25ba0414cddaf
SangJLee1103 <jesusfollow1103@naver.com>
2023-09-21 22:57:57 +0900

    Initial Commit

---
+++ MovieYa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieYa
+//
+//  Created by 이상준 on 2023/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
